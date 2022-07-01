30d08d232f10b982c27d561169ab0881a30d75f3
talha wassan <talha.wassan22@gmail.com>
2022-07-02 01:32:48 +0500

    feat: node modules added

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = 4000
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`)
+})
+
